654b36e8fcf9fc78c6ddf91119bb3e282c514a72
jeunlee <junniesu@gmail.com>
2017-04-10 15:20:50 -0400

    tests into 1 file

--- vector-class/vector.py
+++ vector-class/vector.py
@@ -96,30 +96,6 @@ class Vector(object):
         return self.dotProduct(w) == 0
 
 
-# hey = Vector([1, 2])
-# print(hey.plus(Vector([10, 1])))
-# print(hey.scalarMultiply(3))
-# print(hey.minus(Vector([10, 1])))
-# print(hey)
-# print(hey.__eq__(Vector([1, 0])))
-
-# 1. quiz: plus, minus, multiply
-# print(Vector([8.218, -9.341]).plus(Vector([-1.129, 2.111])))
-# print(Vector([7.119, 8.215]).minus(Vector([-8.223, 0.878])))
-# print(Vector([1.671, -1.012, -0.318]).scalarMultiply(7.41))
-
-# 2. quiz: magnitude, normalization
-# print(Vector([-0.221, 7.437]).magnitude())
-# print(Vector([8.813, -1.331, -6.247]).magnitude())
-# print(Vector([5.581, -2.136]).direction())
-# print(Vector([1.996, 3.108, -4.554]).direction())
-
-# 3. quiz: dot products
-# print(Vector([7.887, 4.138]).dotProduct(Vector([-8.802, 6.776])))
-# print(Vector([-5.955, -4.904, -1.874]).dotProduct(Vector([-4.496, -8.755, 7.103])))
-# print(Vector([3.183, -7.627]).angle(Vector([-2.668, 5.319]), True, True))
-# print(Vector([7.35, 0.221, 5.188]).angle(Vector([2.751, 8.259, 3.985]), True, False))
-
 # 4. quiz: parallel or orthogonal
 print(Vector([-7.579, -7.88]).checkParallel(Vector([22.737, 23.64])))
 print(Vector([-7.579, -7.88]).checkOrthogonal(Vector([22.737, 23.64])))
